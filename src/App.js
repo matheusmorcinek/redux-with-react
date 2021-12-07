66aad44dadd097e87f54dd957cf139dbb2df3cf0
Matheus Morcinek <matheusmorcinek@gmail.com>
2021-12-07 19:58:16 -0300

    adding video component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Sidebar from "./components/Sidebar";
+import Video from "./components/Video";
 
 const App = () => {
   return (
     <div className="App">
       <h1>React and Redux</h1>
+      <Video />
       <Sidebar />
     </div>
   );
